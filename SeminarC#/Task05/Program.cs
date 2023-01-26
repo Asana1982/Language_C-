c3abb02f336c66f23a1d48e66730556ff07adff5
Asanovich <asansvetlana@yandex.ru>
2023-01-26 16:22:36 +0300

    Вывод чисел в промежутке от - до +

---
+++ SeminarC#/Task05/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), count = (-1) * n;
+while (count <= n)
+{
+    Console.Write($"{count} ");
+    count++; // count = count + 1;
+}
